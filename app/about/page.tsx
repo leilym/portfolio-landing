189ed14d204bcca9567b9359302ffaeab93d3bd7
leilym <59461029+leilym@users.noreply.github.com>
2025-08-16 13:07:22 -0400

    about me page updated with colors and wording

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -22,28 +22,29 @@ export default function AboutPage() {
 
         <div className="prose prose-lg prose-invert">
           <p>
-            Hi, I'm Leili. I'm a Creative Technologist and Data Enthusiast with a passion for combining art, humanity, and
+            Hi, I'm Leili. I'm a Creative Technologist with a passion for combining art, humanity, and
             technology.
-          </p>
+          </p> 
 
-          <p>
-          My non-linear journey has taken me across countries, careers, and creative paths. 
-          I left my home country at 16 to attend United World College Dilijan in Armenia, 
+          <p><br></br>
+          My non-linear journey has taken me across countries, careers, and creative paths. <br></br>
+          <br></br>I left my home country at 16 in hopes of exploring a world not confined by the geography of my birthplace, to attend United World College Dilijan in Armenia, 
           then moved to the U.S. to earn my B.A. at Middlebury College in Vermont, where I majored in Computer Science and minored in Religion.
-          
+          <br></br>
+          <br></br>
           </p>
           <p>
           My career began at OneReach.ai as a Solutions Analyst, but after being laid off, I pivoted into teaching; spending a year guiding students through AP Computer Science Principles. 
-          
+          <br></br>
           Today, I am part of the Implementation team at Intapp DealCloud.
-          
+          <br></br>
           Alongside my professional work, I am passionate about art, community, and impact. 
           I have volunteered for several years as part of the UWC Selection process, helping students embark on their own transformative journeys.
-          Right now, I am exploring my passion for art through digital illustration and writing, using creativity to spark connection and reflection. 
-          You can find my artwork at: https://www.instagram.com/art.from.themundane/
-          My blogs at: https://medium.com/@leyli-s-life-drafts
+          <br></br>Right now, I am exploring my passion for art through digital illustration and writing, using creativity to spark connection and reflection. <br></br>
+          <br></br>You can find my artwork at: <p style={{ color: '#DFC98A' }}>https://www.instagram.com/art.from.themundane/</p>
+          <br></br>My blogs at: <p style={{ color: '#DFC98A' }}> https://medium.com/@leyli-s-life-drafts </p>
 
-          
+
           </p>
           
         </div>

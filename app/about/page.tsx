a67c6f7706efcb69a74c19a05b269bc96e64095c
leilym <59461029+leilym@users.noreply.github.com>
2025-08-16 12:56:48 -0400

    about me page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -22,28 +22,30 @@ export default function AboutPage() {
 
         <div className="prose prose-lg prose-invert">
           <p>
-            Hi, I'm Leili. I'm a Creative Technologist and Data Enthusiast with a passion for combining art and
+            Hi, I'm Leili. I'm a Creative Technologist and Data Enthusiast with a passion for combining art, humanity, and
             technology.
           </p>
 
           <p>
-            My background spans across design, development, and data analysis, allowing me to approach problems from
-            multiple perspectives and create innovative solutions.
+          My non-linear journey has taken me across countries, careers, and creative paths. 
+          I left my home country at 16 to attend United World College Dilijan in Armenia, 
+          then moved to the U.S. to earn my B.A. at Middlebury College in Vermont, where I majored in Computer Science and minored in Religion.
+          
           </p>
-
           <p>
-            When I'm not coding or analyzing data, you can find me sketching, exploring new design trends, or
-            experimenting with new technologies.
-          </p>
+          My career began at OneReach.ai as a Solutions Analyst, but after being laid off, I pivoted into teaching; spending a year guiding students through AP Computer Science Principles. 
+          
+          Today, I am part of the Implementation team at Intapp DealCloud.
+          
+          Alongside my professional work, I am passionate about art, community, and impact. 
+          I have volunteered for several years as part of the UWC Selection process, helping students embark on their own transformative journeys.
+          Right now, I am exploring my passion for art through digital illustration and writing, using creativity to spark connection and reflection. 
+          You can find my artwork at: https://www.instagram.com/art.from.themundane/
+          My blogs at: https://medium.com/@leyli-s-life-drafts
 
-          <h2>Skills</h2>
-          <ul>
-            <li>UX/UI Design</li>
-            <li>Front-end Development</li>
-            <li>Data Visualization</li>
-            <li>Creative Coding</li>
-            <li>Digital Illustration</li>
-          </ul>
+          
+          </p>
+          
         </div>
       </motion.div>
     </div>
